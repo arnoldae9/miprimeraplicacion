8910cd0a11cfb79cb78f9cb589ef750af494e0a0
arnold <arnold@DESKTOP-J3DKC2P>
2020-07-25 18:04:08 -0500

    Agregar archivos de proyecto.

---
+++ miprimeraplicacion/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miprimeraplicacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {   
+
+
+
+
+
+
+
+            /*
+            string cad1 = "ABC";
+            string cad2 = "DEF";
+            int num = 20;
+
+            string sumacadena = cad1 + cad2;
+            string multicadena = cad1 +num;
+
+            Console.WriteLine(sumacadena);
+            Console.WriteLine(multicadena);
+
+            Console.ReadLine();
+            */
+            
+            
+            /*
+            int a = 34;
+            int b = 56;
+            double c = 20.5;
+
+            int suma = a + b;
+            int resta = a - b;
+            int multiplicacion = a * b;
+            double division = (a*1.0) / b;
+
+            Console.WriteLine("la suma es: " + suma);
+            Console.WriteLine("la resta es: " + resta);
+            Console.WriteLine("la multiplicacion es: " + multiplicacion);
+            Console.WriteLine("la division es: " + division);
+
+            Console.ReadLine();
+            */
+            
+            /*
+            int a = 2000;
+            short b = -20;
+            long c = 1000000;
+
+            double d = 1212.341241241;
+
+            Console.WriteLine(d);
+            Console.ReadLine();
+            */
+        }
+    }
+}
